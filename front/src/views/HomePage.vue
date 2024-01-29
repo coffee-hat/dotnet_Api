15d81c09f162c149fe99ac914b3d22eeadbffbbb
coffee-hat <schramhugo77@gmail.com>
2024-01-29 19:08:06 +0100

    add base front

---
+++ front/src/views/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <Element title="Department"/>
+
+  <Element title="Employee"/>
+
+  <Element title="A"/>
+
+  <Element title="B"/>
+</template>
+
+<script setup>
+import Element from '@/components/Element.vue'
+</script>
+
+<style scoped>
+/* Apply Tailwind CSS classes here */
+</style>
